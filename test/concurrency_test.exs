e4967ab56752f75c14c20ed76b98ccab5739182c
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-08 20:20:03 +0200

    :white_check_mark: updoot tests

--- test/concurrency_test.exs
+++ test/concurrency_test.exs
@@ -1,28 +1,29 @@
 defmodule ConcurrencyTest do
   use ExUnit.Case
-  import Venomous.SnakeWrapper, only: [python: 3, python: 4, get_snakes_ready: 1]
+  import Venomous.SnakeArgs
+  import Venomous.SnakeWrapper
+  # @tag timeout: :infinity
+  # test "stress snakes" do
+  #   sum_that =
+  #     1..1000
+  #     |> Enum.map(fn _ ->
+  #       Task.async(fn ->
+  #         python(:builtins, :sum, [[1, 1]])
+  #       end)
+  #     end)
+  #     |> Task.await_many(:infinity)
+  #     |> Enum.sum()
 
-  @tag timeout: :infinity
-  test "stress snakes" do
-    sum_that =
-      1..1000
-      |> Enum.map(fn _ ->
-        Task.async(fn ->
-          python(:builtins, :sum, [[1, 1]])
-        end)
-      end)
-      |> Task.await_many(:infinity)
-      |> Enum.sum()
-
-    assert sum_that == 2 * 1000
-  end
+  #   assert sum_that == 2 * 1000
+  # end
 
   test "concurrent python processes" do
     Process.send_after(self(), :fail, 5_000)
+    args = snake_args(:time, :sleep, [2])
 
     1..100
     |> Enum.map(fn _ ->
-      Task.async(fn -> python(:time, :sleep, [2]) end)
+      Task.async(fn -> python(args) end)
     end)
     |> Task.await_many(:infinity)
 
--- test/venomous_test.exs
+++ test/venomous_test.exs
@@ -2,7 +2,8 @@ defmodule VenomousTest do
   alias Venomous.SnakeError
   alias Venomous.SnakeSupervisor
   use ExUnit.Case
-  import Venomous.SnakeWrapper, only: [python: 3, python: 4]
+  import Venomous.SnakeWrapper
+  import Venomous.SnakeArgs
   doctest Venomous
 
   test "greets the world" do
@@ -10,9 +11,16 @@ defmodule VenomousTest do
   end
 
   test "reuse alive snakes" do
-    assert python(:builtins, :round, [0.9]) == 1
-    assert python(:builtins, :round, [0.9]) == 1
-    assert python(:builtins, :round, [0.9]) == 1
+    round_args =
+      snake_args(
+        :builtins,
+        :round,
+        [0.9]
+      )
+
+    assert python(round_args) == 1
+    assert python(round_args) == 1
+    assert python(round_args) == 1
 
     snakes = DynamicSupervisor.count_children(SnakeSupervisor) |> Map.get(:workers)
     assert snakes == 1
@@ -20,28 +28,23 @@ defmodule VenomousTest do
 
   test "python builtin func" do
     neg_number = :rand.uniform() * -1_000
-    assert Venomous.SnakeWrapper.python(:builtins, :abs, [neg_number]) == abs(neg_number)
-    assert python(:builtins, :round, [1.5]) == 2
+    assert snake_args(:builtins, :abs, [neg_number]) |> python() |> Kernel.==(abs(neg_number))
+    assert snake_args(:builtins, :round, [1.5]) |> python() |> Kernel.==(2)
   end
 
   test "python timeout" do
     result =
-      try do
-        python(:time, :sleep, [0.5], 100)
-      catch
-        :exit, _ -> :ok
-        _ -> :fail
-      end
+      snake_args(:time, :sleep, [0.5]) |> python(100)
 
-    assert result == :ok
+    assert result == %{error: "timeout"}
   end
 
   test "snake errors" do
-    sleepy_snake = python(:time, :sleep, ["sleepy snake"])
-    slithering_across = python(:snake, :slithering, ["across"])
-    hilarious = python("this", "is", "hilariousssssss")
-    assert match?(sleepy_snake, %SnakeError{})
-    assert match?(slithering_across, %SnakeError{})
-    assert match?(hilarious, %FunctionClauseError{})
+    _sleepy_snake = snake_args(:time, :sleep, ["sleepy snake"]) |> python()
+    _slithering_across = snake_args(:snake, :slithering, ["across"]) |> python()
+    _hilarious = snake_args("this", "is", "hilariousssssss") |> python()
+    assert match?(_sleepy_snake, %SnakeError{})
+    assert match?(_slithering_across, %SnakeError{})
+    assert match?(_hilarious, %FunctionClauseError{})
   end
 end
