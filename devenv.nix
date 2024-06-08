6970d2f40ef1e5241da71c4e72b8e10ac4f5013c
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-06-08 02:04:06 +0200

    :snowflake: nix stuff

---
+++ devenv.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+
+  packages = with pkgs; [
+    elixir_1_16
+    erlang
+    elixir-ls
+    python311
+    python311Packages.pip
+    nodePackages.pyright
+    
+ ] ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [inotify-tools]);
+
+  env.LANG = "en_US.UTF-8";
+  dotenv.enable = true;
+}
