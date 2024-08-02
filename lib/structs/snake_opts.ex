72a971e9cd055e6e21edfde8470a438600852ad5
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-08-03 00:50:48 +0200

    :sparkles: Add snake opts

---
+++ lib/structs/snake_opts.ex
@@ -0,0 +1,36 @@
+defmodule Venomous.SnakeOpts do
+  @available_opts [
+    :module_paths,
+    :cd,
+    :compressed,
+    :envvars,
+    :packet_bytes,
+    :python_executable
+  ]
+
+  defp construct_python_opts({:module_paths, paths}) when is_list(paths) do
+    {:python_path, Enum.map(paths, &to_charlist(&1))}
+  end
+
+  defp construct_python_opts({:module_paths, path}) when is_binary(path) do
+    {:python_path, to_charlist(path)}
+  end
+
+  defp construct_python_opts({:envvars, vars}) when is_list(vars) do
+    {:env, Enum.map(vars, fn {key, val} -> {to_charlist(key), to_charlist(val)} end)}
+  end
+
+  defp construct_python_opts({:python_executable, path}) when is_binary(path) do
+    {:python, to_charlist(path)}
+  end
+
+  defp construct_python_opts({:packet_bytes, bytes}) when is_integer(bytes) do
+    {:packet, bytes}
+  end
+
+  defp construct_python_opts(keyword), do: keyword
+
+  def to_erlport_opts(opts) do
+    opts |> Keyword.take(@available_opts) |> Keyword.new(&construct_python_opts(&1))
+  end
+end
