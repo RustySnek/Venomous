133769e3f36342f37f1917940dfc2df6ee7ea6b2
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-08-03 01:51:45 +0200

    :zap: Add serpent watcher to application

--- lib/application.ex
+++ lib/application.ex
@@ -1,11 +1,17 @@
 defmodule Venomous.Application do
   @moduledoc """
-    Initializes Snake Managers and handles config
+    Initializes Snake Manager/Supervisor/Watcher and handles config
   """
   use Application
   @default_ttl_minutes 15
   @default_cleaner_interval 60_000
   @default_perpetual_workers 10
+  @default_serpent_config [
+    blacklist: [],
+    module: :serpent_watcher,
+    func: :watch_directories,
+    args: [Venomous.SnakeManager]
+  ]
 
   def start(_type, _args) do
     children =
@@ -21,13 +27,15 @@ defmodule Venomous.Application do
           id: Venomous.PetSnakeManager,
           restart: :permanent
         )
-      ] ++ snake_supervisor_spec()
+      ] ++
+        snake_supervisor_spec() ++
+        watch_serpent_spec()
 
     opts = [strategy: :one_for_one, name: Venomous.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  defp snake_manager_specs() do
+  defp snake_manager_specs do
     table = :ets.new(:snake_terrarium, [:set, :public])
 
     config = Application.get_env(:venomous, :snake_manager, %{})
@@ -39,22 +47,45 @@ defmodule Venomous.Application do
     cleaner_interval_ms =
       Map.get(config, :cleaner_interval, @default_cleaner_interval)
 
-    python_opts =
-      config
-      |> Map.get(:python_opts, [])
-      |> Keyword.merge(erlport_encoder: Map.get(config, :erlport_encoder, %{}))
+    reload_module = Map.get(config, :reload_module, :reload)
+
+    python_opts = python_opts(config)
 
     %{
       table: table,
       snake_ttl_minutes: snake_ttl,
       perpetual_workers: perpetual_workers,
       cleaner_interval_ms: cleaner_interval_ms,
-      python_opts: python_opts
+      python_opts: python_opts,
+      reload_module: reload_module
     }
   end
 
-  defp snake_supervisor_spec() do
-    if Application.get_env(:venomous, :snake_supervisor_enabled, false) do
+  defp watch_serpent_spec do
+    config = Application.get_env(:venomous, :serpent_watcher, [])
+
+    case Keyword.get(config, :enable, false) do
+      true ->
+        serpent_opts =
+          python_opts()
+          |> Keyword.merge(@default_serpent_config)
+          |> Keyword.merge(config)
+
+        [
+          Supervisor.child_spec(
+            {Venomous.SerpentWatcher, serpent_opts},
+            id: Venomous.SerpentWatcher,
+            restart: :permanent
+          )
+        ]
+
+      false ->
+        []
+    end
+  end
+
+  defp snake_supervisor_spec do
+    if Application.get_env(:venomous, :test_snake_supervisor, false) do
       [
         {Venomous.SnakeSupervisor, [strategy: :one_for_one, max_restarts: 0, max_children: 50]},
         {Venomous.PetSnakeSupervisor, [strategy: :one_for_one, max_children: 10]}
@@ -62,5 +93,11 @@ defmodule Venomous.Application do
     else
       []
     end
+  end
+
+  defp python_opts(env \\ Application.get_env(:venomous, :snake_manager, %{})) do
+    env
+    |> Map.get(:python_opts, [])
+    |> Keyword.merge(erlport_encoder: Map.get(env, :erlport_encoder, %{}))
   end
 end
