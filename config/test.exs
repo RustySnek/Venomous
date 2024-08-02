d269dd0307cec250fe73107ad8f619819f6eeb55
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-08-03 01:52:19 +0200

    :wrench: change test config

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,16 @@
 import Config
 
-config :venomous, :snake_supervisor_enabled, true
+config :venomous, :test_snake_supervisor, true
+
+config :venomous,
+  serpent_watcher: [
+    enable: true,
+    blacklist: ["meow"]
+  ]
 
 config :venomous, :snake_manager, %{
-  erlport_encoder: %{}
+  erlport_encoder: %{},
+  python_opts: [
+    module_paths: ["python/", ".devenv/state/venv/lib/python3.11/site-packages"]
+  ]
 }
