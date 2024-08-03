8f9d4ba834f7c692ed903d8dd1f3ba15c6474653
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-08-03 04:03:29 +0200

    :wrench: updoot test cfg

--- config/test.exs
+++ config/test.exs
@@ -4,10 +4,12 @@ config :venomous, :test_snake_supervisor, true
 
 config :venomous,
   serpent_watcher: [
-    enable: true,
-    blacklist: ["meow"]
+    enable: true
   ]
 
 config :venomous, :snake_manager, %{
-  erlport_encoder: %{}
+  erlport_encoder: %{},
+  python_opts: [
+    module_paths: ["python/"]
+  ]
 }
