8f60f725d56a9e5b63e9698427bffc6051223d99
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-08-03 17:04:44 +0200

    :ambulance: fix submodules

--- python/reload.py
+++ python/reload.py
@@ -5,8 +5,9 @@ from types import ModuleType
 
 def reload(module: bytes):
     for _importer, modname, _ispkg in pkgutil.iter_modules():
-        if module.decode().startswith(modname):
-            mod = importlib.import_module(modname)
+        module_name = module.decode()
+        if module_name.startswith(modname):
+            mod = importlib.import_module(module_name)
             if isinstance(mod, ModuleType):
                 importlib.reload(mod)
                 return
