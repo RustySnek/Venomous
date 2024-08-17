a738827fdc4f169d3b98efc4931d0c3ebe7e584f
Pascal Jodlowski <jodlowskipascal@gmail.com>
2024-08-17 23:32:57 +0200

    :wrench: ignore task

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,6 @@
 [
   {"lib/mix/tasks/watchdog.ex", :unknown_function},
-  {"lib/mix/tasks/watchdog.ex", :callback_info_missing}
+  {"lib/mix/tasks/watchdog.ex", :callback_info_missing},
+  {"lib/mix/tasks/structs.ex", :unknown_function},
+  {"lib/mix/tasks/structs.ex", :callback_info_missing}
 ]
